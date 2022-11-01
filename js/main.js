35deea66b45d73c533ac9fa1c178b8b116baae12
Tarek Chaaban <Chaaban_tarek@yahoo.com>
2022-11-01 03:12:19 +0000

    work in progress, finsihed up until reset image previews src attribute.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,25 @@
-// var $photoUrl = document.querySelector('#photo-url');
-// var $entryImage = document.querySelector('.entry-image');
-// var $journalEntryForm = document.querySelector('.entry-form');
-// $journalEntryForm.addEventListener('submit', submitHandler);
-// $photoUrl.addEventListener('input', photoHandler);
+var $photoUrl = document.querySelector('#photo-url');
+var $entryImage = document.querySelector('.entry-image');
+var $journalEntryForm = document.querySelector('.entry-form');
+$journalEntryForm.addEventListener('submit', submitHandler);
+$photoUrl.addEventListener('input', photoHandler);
 
-// function photoHandler(event) {
-//   $entryImage.setAttribute('src', $photoUrl.value);
-// }
+function photoHandler(event) {
+  $entryImage.setAttribute('src', $photoUrl.value);
+}
+
+function submitHandler(event) {
+  event.preventDefault();
+  var $title = $journalEntryForm.querySelector('input#title');
+  var $photoUrl = $journalEntryForm.querySelector('input#photo-url');
+  var $notes = $journalEntryForm.querySelector('input#notes');
+  var obj = {
+    title: $title.value,
+    photoUrl: $photoUrl.value,
+    notes: $notes.value,
+    nextEntryId: data.nextEntryId
+  };
+  data.nextEntryId += 1;
+
+  data.entries.push(obj);
+}

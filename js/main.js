3fcfe58ff5ffe978581803fb78b66e24c8bc02b6
Tarek Chaaban <Chaaban_tarek@yahoo.com>
2022-11-01 00:04:46 +0000

    working on subtmit handler function.

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// var $photoUrl = document.querySelector('#photo-url');
+// var $entryImage = document.querySelector('.entry-image');
+// var $journalEntryForm = document.querySelector('.entry-form');
+// $journalEntryForm.addEventListener('submit', submitHandler);
+// $photoUrl.addEventListener('input', photoHandler);
+
+// function photoHandler(event) {
+//   $entryImage.setAttribute('src', $photoUrl.value);
+// }

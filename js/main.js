f57d88296eb5ab974ec4e72f600a02392d8724bf
Tarek Chaaban <Chaaban_tarek@yahoo.com>
2022-11-01 04:29:42 +0000

    completed Issue-1-create-an-entry.

--- js/main.js
+++ js/main.js
@@ -12,14 +12,21 @@ function submitHandler(event) {
   event.preventDefault();
   var $title = $journalEntryForm.querySelector('input#title');
   var $photoUrl = $journalEntryForm.querySelector('input#photo-url');
-  var $notes = $journalEntryForm.querySelector('input#notes');
+  var $notes = $journalEntryForm.querySelector('textarea#notes');
   var obj = {
     title: $title.value,
     photoUrl: $photoUrl.value,
     notes: $notes.value,
-    nextEntryId: data.nextEntryId
+    Id: data.nextEntryId
   };
   data.nextEntryId += 1;
+  data.entries.unshift(obj);
+  $entryImage.setAttribute('src', 'images/placeholder-image-square.jpg');
+  event.target.reset();
+}
 
-  data.entries.push(obj);
+window.addEventListener('beforeunload', unloadHandler);
+function unloadHandler(event) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('data', dataJSON);
 }

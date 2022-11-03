4004e2dc14be69b5fa3a401d643f75103c5f88df
Tarek Chaaban <Chaaban_tarek@yahoo.com>
2022-11-03 00:07:28 +0000

    feature 3 work in progress.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 var $photoUrl = document.querySelector('#photo-url');
 var $entryImage = document.querySelector('.entry-image');
 var $journalEntryForm = document.querySelector('.entry-form');
+var $title = $journalEntryForm.querySelector('#title');
+var $notes = $journalEntryForm.querySelector('#notes');
 $journalEntryForm.addEventListener('submit', submitHandler);
 $photoUrl.addEventListener('input', photoHandler);
 
@@ -9,29 +11,36 @@ function photoHandler(event) {
 }
 
 function submitHandler(event) {
-  event.preventDefault();
-  var $title = $journalEntryForm.querySelector('input#title');
-  var $photoUrl = $journalEntryForm.querySelector('input#photo-url');
-  var $notes = $journalEntryForm.querySelector('textarea#notes');
-  var obj = {
-    title: $title.value,
-    photoUrl: $photoUrl.value,
-    notes: $notes.value,
-    Id: data.nextEntryId
-  };
-  data.nextEntryId += 1;
-  data.entries.unshift(obj);
-  $entryImage.setAttribute('src', 'images/placeholder-image-square.jpg');
-  var newEntry = renderEntry(obj);
-  $unorderedListRow.prepend(newEntry);
-  $entriesView.className = 'entries';
-  $formView.className = 'entry-form hidden';
-  event.target.reset();
+  if (data.editing === null) {
+    event.preventDefault();
+    var obj = {
+      title: $title.value,
+      photoUrl: $photoUrl.value,
+      notes: $notes.value,
+      Id: data.nextEntryId
+    };
+    data.nextEntryId += 1;
+    data.entries.unshift(obj);
+    $entryImage.setAttribute('src', 'images/placeholder-image-square.jpg');
+    var newEntry = renderEntry(obj);
+    $unorderedListRow.prepend(newEntry);
+    $entriesView.className = 'entries';
+    $formView.className = 'entry-form hidden';
+    event.target.reset();
+  } else {
+    for (var i = 0; i < data.entries.length; i++) {
+      if (data.entries[i].Id === data.editing) {
+        data.entries.splice(i, 1);
+      }
+    }
+  }
+
 }
 
 function renderEntry(obj) {
   var $columnFullList = document.createElement('li');
   $columnFullList.className = 'column-full';
+  $columnFullList.setAttribute('data-entry-id', obj.Id);
 
   var $divRow = document.createElement('div');
   $divRow.className = 'row';
@@ -43,10 +52,13 @@ function renderEntry(obj) {
   $image.setAttribute('src', obj.photoUrl);
 
   var $divColumnHalfText = document.createElement('div');
-  $divColumnHalfText.className = 'column-half';
+  $divColumnHalfText.className = 'column-half relative';
 
   var $entryTitle = document.createElement('h2');
   $entryTitle.textContent = obj.title;
+
+  var $editButton = document.createElement('i');
+  $editButton.className = 'fa-solid fa-pen';
 
   var $entryNotes = document.createElement('p');
   $entryNotes.textContent = obj.notes;
@@ -56,6 +68,7 @@ function renderEntry(obj) {
   $divColumnHalfImage.appendChild($image);
   $divRow.appendChild($divColumnHalfText);
   $divColumnHalfText.appendChild($entryTitle);
+  $divColumnHalfText.appendChild($editButton);
   $divColumnHalfText.appendChild($entryNotes);
 
   return $columnFullList;
@@ -95,4 +108,23 @@ function formViewHandler(event) {
   $formView.className = 'entry-form';
   $entriesView.className = 'entries hidden';
   data.view = 'entry-form';
+  data.editing = null;
+}
+
+$unorderedListRow.addEventListener('click', editHandler);
+function editHandler(event) {
+  if (event.target.tagName === 'I') {
+    $formView.className = 'entry-form';
+    $entriesView.className = 'entries hidden';
+    for (var i = 0; i < data.entries.length; i++) {
+      var attributeNumberString = event.target.closest('.column-full').getAttribute('data-entry-id');
+      if (parseInt(attributeNumberString) === data.entries[i].Id) {
+        data.editing = data.entries[i].Id;
+        $title.value = data.entries[i].title;
+        $photoUrl.value = data.entries[i].photoUrl;
+        $entryImage.setAttribute('src', data.entries[i].photoUrl);
+        $notes.value = data.entries[i].notes;
+      }
+    }
+  }
 }
